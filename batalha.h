06b4279dd3cfe0145c9a9e734a664eb2638f5df3
LLR794 <131215813+LLR794@users.noreply.github.com>
2025-08-22 11:34:01 -0300

    Create batalha.h

---
+++ batalha.h
@@ -0,0 +1,10 @@
+#ifndef BATALHA_H
+#define BATALHA_H
+
+#include "territorio.h"
+
+// Protótipo da função de ataque
+// Recebe ponteiros para os territórios atacante e defensor para modificar seus dados
+void simular_ataque(Territorio *atacante, Territorio *defensor);
+
+#endif // BATALHA_H
